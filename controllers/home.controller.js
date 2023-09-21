07568b945f9c687544efbb03c4160132d8cbf020
Aurélien <aurelien.strimelle@bstorm.be>
2023-09-21 15:41:46 +0200

    Add View 😎

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,14 +1,44 @@
+const ejs = require('ejs');
 
 const homeController = {
 
     index: (req, res) => {
         //! Page d'accueil → Liste des message
 
+        // "require.main.path" permet d'obtenir le repertoire racine
+
+        // Rendu de la page (Callback)
+        ejs.renderFile(`${require.main.path}/views/home/index.ejs`, (error, pageRender) => {
+            //* Erreur lors de la génération du rendu
+            if(error) {
+                console.error(error);
+
+                res.writeHead(500);
+                res.end();
+                return;
+            }
+
+            //* Génération de la vue réussi !
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(pageRender);
+        });
     },
 
     messageGET: (req, res) => {
         //! Page de formulaire
 
+        // Rendu de la page (Promise)
+        ejs.renderFile(`${require.main.path}/views/home/message-form.ejs`)
+            .then(pageRender => {
+                //* Génération de la vue réussi !
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end(pageRender);
+            })
+            .catch(error => {
+                //* Erreur lors de la génération du rendu
+                res.writeHead(500);
+                res.end();
+            });
     },
 
     messagePOST: (req, res) => {
