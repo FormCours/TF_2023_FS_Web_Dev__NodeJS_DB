f35797ff73fd547cc6cdba937610168ca1bf6808
Aurélien <aurelien.strimelle@bstorm.be>
2023-09-21 17:33:48 +0200

    Add Request SQL 🐱‍👓

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,14 +1,30 @@
 const ejs = require('ejs');
+const querystring = require('querystring');
+const mssql = require('mssql')
+const { createDbConnection } = require('../utils/db.utils');
 
 const homeController = {
 
-    index: (req, res) => {
+    index: async (req, res) => {
         //! Page d'accueil → Liste des message
 
-        // "require.main.path" permet d'obtenir le repertoire racine
-
+        // Récuperation des données depuis la DB
+        const db = await createDbConnection();
+        const result = await db.query('SELECT * FROM Message')
+        
+        // Formattage des données pour l'utilisation (mapping)
+        const messages = result.recordset.map(row => {
+            return {
+                id : row['MessageId'],
+                pseudo: row['Pseudo'],
+                content: row['Content'],
+                createDate: row['CreateDate']
+            }
+        });
+        
         // Rendu de la page (Callback)
-        ejs.renderFile(`${require.main.path}/views/home/index.ejs`, (error, pageRender) => {
+        //? Pour obtenir le repertoire racine → "require.main.path" 
+        ejs.renderFile(`${require.main.path}/views/home/index.ejs`, { messages }, (error, pageRender) => {
             //* Erreur lors de la génération du rendu
             if(error) {
                 console.error(error);
@@ -43,7 +59,36 @@ const homeController = {
 
     messagePOST: (req, res) => {
         //! Traitement des données du formulaire
+        
+        //* Récuperer les données du POST
+        let body = '';
+        req.on('data', formData => {
+            body += formData.toString();
+        });
 
+        //* Traitement après la récéption des données
+        req.on('end', async () => {
+            const data = querystring.parse(body);
+            console.log(body);
+            console.log(data);
+
+            const db = await createDbConnection();
+
+            //! Requete SQL préparé, pour éviter la fail de sécurité "Injection SQL"
+            const sqlQuery = new mssql.PreparedStatement(db);
+            // Définition des types de parametres
+            sqlQuery.input('pseudo', mssql.NVarChar);
+            sqlQuery.input('content', mssql.NVarChar);
+            sqlQuery.input('email', mssql.NVarChar);
+            // Préparation de la requete
+            await sqlQuery.prepare('INSERT INTO [Message](Pseudo, Content, Email) VALUES(@pseudo, @content, @email)');
+            // Execution de la requete
+            await sqlQuery.execute(data);
+
+            //! Redirection vers la page d'accueil
+            res.writeHead(302, { location: '/' });
+            res.end();
+        })
     }
 };
 
